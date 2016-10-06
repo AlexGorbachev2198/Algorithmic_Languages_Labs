a80fa973aeb4ff942c2c91833381d01d4d715c70
AlexGorbachev2198 <Gorbachev2198@gmail.com>
2016-10-06 20:36:12 +0300

    Update Lab1_1.cpp

--- Lab1_1.cpp
+++ Lab1_1.cpp
@@ -6,10 +6,10 @@ int main()
     int X;
     double result;
     std::cout  << "X=";
-    std::cin >> X;
-    if((X == -2) || (X == 2)) std::cout  << "X не входит в ОДЗ";
+    std::cin >> X;//Ввод значений
+    if((X == -2) || (X == 2)) std::cout  << "X не входит в ОДЗ";//Проверка ОДЗ
     else {
-        result = sqrt(pow(X, 2) - 4) / ((2 + X) * (pow(X, 3) - 8));
+        result = sqrt(pow(X, 2) - 4) / ((2 + X) * (pow(X, 3) - 8));//Решение
         std::cout << result;
     }
 }
