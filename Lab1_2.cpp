dbc251e632018b74b9ef29ef2275b96fd668e777
AlexGorbachev2198 <Gorbachev2198@gmail.com>
2016-10-06 20:38:02 +0300

    Update Lab1_2.cpp

--- Lab1_2.cpp
+++ Lab1_2.cpp
@@ -7,20 +7,20 @@ int main() {
     {
         while(R <= 0)
         {
-            std::cout << "Неверный радиус. Введите другой радиус:"<< std::endl; std::cin >> R;
+            std::cout << "Неверный радиус. Введите другой радиус:"<< std::endl; std::cin >> R;//Проверка правильности введенного радиуса
         }
     }
     std::cout << "Введите X центра:"<< std::endl; std::cin >> X;
     std::cout << "Введите Y центра:"<< std::endl; std::cin >> Y;
     std::cout << "Введите X точки:"<< std::endl; std::cin >> X1;
-    std::cout << "Введите Y точки:0"<< std::endl; std::cin >> Y1;
+    std::cout << "Введите Y точки:0"<< std::endl; std::cin >> Y1; //Ввод значений
     if(X1 <= X + R)
     {
         if(X1 >= X - R)
         {
             if(Y1 <= Y + R)
             {
-                if(Y1 >= Y - R) std::cout <<"Точка находится в окружности"<< std::endl;
+                if(Y1 >= Y - R) std::cout <<"Точка находится в окружности"<< std::endl; //Проверка и вывод результата
             }
             else std::cout <<"Точка не находится в окружности"<< std::endl;
         }
