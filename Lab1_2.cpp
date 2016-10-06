f75c4765b2ec81b99db30234ee8cfab632485427
AlexGorbachev2198 <Gorbachev2198@gmail.com>
2016-10-06 21:35:36 +0300

    Находится ли точка в квадрате

--- Lab1_2.cpp
+++ Lab1_2.cpp
@@ -2,30 +2,30 @@
 
 int main() {
     int R,X,Y,X1,Y1;
-    std::cout << "Введите радиус окружности:"<< std::endl; std::cin >> R;
+    std::cout << "Введите сторону квадрата:"<< std::endl; std::cin >> R;
     if(R <= 0)
     {
         while(R <= 0)
         {
-            std::cout << "Неверный радиус. Введите другой радиус:"<< std::endl; std::cin >> R;//Проверка правильности введенного радиуса
+            std::cout << "Неверная сторона. Введите другую:"<< std::endl; std::cin >> R;//Проверка правильности введенной стороны
         }
     }
-    std::cout << "Введите X центра:"<< std::endl; std::cin >> X;
-    std::cout << "Введите Y центра:"<< std::endl; std::cin >> Y;
+    std::cout << "Введите X левой нижней вершины:"<< std::endl; std::cin >> X;
+    std::cout << "Введите Y левой нижней вершины:"<< std::endl; std::cin >> Y;
     std::cout << "Введите X точки:"<< std::endl; std::cin >> X1;
     std::cout << "Введите Y точки:"<< std::endl; std::cin >> Y1; //Ввод значений
     if(X1 <= X + R)
     {
-        if(X1 >= X - R)
+        if(X1 >= X)
         {
             if(Y1 <= Y + R)
             {
-                if(Y1 >= Y - R) std::cout <<"Точка находится в окружности"<< std::endl; //Проверка и вывод результата
+                if(Y1 >= Y) std::cout <<"Точка находится в квадрате"<< std::endl; //Проверка и вывод результата
             }
-            else std::cout <<"Точка не находится в окружности"<< std::endl;
+            else std::cout <<"Точка не находится в квадрате"<< std::endl;
         }
-        else std::cout <<"Точка не находится в окружности"<< std::endl;
+        else std::cout <<"Точка не находится в квадрате"<< std::endl;
     }
-    else std::cout <<"Точка не находится в окружности"<< std::endl;
+    else std::cout <<"Точка не находится в квадрате"<< std::endl;
     return 0;
 }
