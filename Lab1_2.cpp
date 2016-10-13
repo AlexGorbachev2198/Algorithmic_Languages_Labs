f91c67c2378ef7e95d3de0c2539dedf98143d3bf
AlexGorbachev2198 <Gorbachev2198@gmail.com>
2016-10-13 20:58:51 +0400

    Находится ли точка в квадрате.

--- Lab1_2.cpp
+++ Lab1_2.cpp
@@ -1,31 +1,32 @@
 #include <iostream>
-
+using namespace std;
 int main() {
     int R,X,Y,X1,Y1;
-    std::cout << "Введите сторону квадрата:"<< std::endl; std::cin >> R;
+    cout << "Введите сторону квадрата"<< endl; cin >> R;
     if(R <= 0)
     {
-        while(R <= 0)
+        while(R <=  0)
         {
-            std::cout << "Неверная сторона. Введите другую:"<< std::endl; std::cin >> R;
+            cout << "Неверная сторона. Введите другую:" << endl; cin >> R;
         }
     }
-    std::cout << "Введите X левой нижней вершины:"<< std::endl; std::cin >> X;
-    std::cout << "Введите Y левой нижней вершины:"<< std::endl; std::cin >> Y;
-    std::cout << "Введите X точки:"<< std::endl; std::cin >> X1;
-    std::cout << "Введите Y точки:"<< std::endl; std::cin >> Y1;
+    cout << "Введите X левой нижней вершины:" << endl; cin >> X;
+    cout << "Введите Y левой нижней вершины:" << endl; cin >> Y;
+    cout << "Введите X точки:" << endl; cin >> X1;
+    cout << "Введите Y точки:" << endl; cin >> Y1;
     if(X1 <= X + R)
     {
-        if(X1 >= X)
+        if(X1 >= X )
         {
             if(Y1 <= Y + R)
             {
-                if(Y1 >= Y) std::cout <<"Точка находится в квадрате"<< std::endl; 
+                if(Y1 >= Y) cout <<"Точка находится в квадрате"<< endl;
+                else cout <<"Точка не находится в квадрате"<< endl;
             }
-            else std::cout <<"Точка не находится в квадрате"<< std::endl;
+            else cout <<"Точка не находится в квадрате"<< endl;
         }
-        else std::cout <<"Точка не находится в квадрате"<< std::endl;
+        else cout <<"Точка не находится в квадрате"<< endl;
     }
-    else std::cout <<"Точка не находится в квадрате"<< std::endl;
+    else cout <<"Точка не находится в квадрате"<< endl;
     return 0;
 }
