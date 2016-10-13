1e2bd792da5b2cfd42540830c63107a664846c48
AlexGorbachev2198 <Gorbachev2198@gmail.com>
2016-10-13 17:33:56 +0400

    Вычисление математического выр-я для данного X.

--- Lab1_1.cpp
+++ Lab1_1.cpp
@@ -6,10 +6,10 @@ int main()
     int X;
     double result;
     std::cout  << "X=";
-    std::cin >> X;//Ввод значений
-    if((X == -2) || (X == 2)) std::cout  << "X не входит в ОДЗ";//Проверка ОДЗ
+    std::cin >> X;
+    if((X == -2) || (X == 2)) std::cout  << "X не входит в ОДЗ";
     else {
-        result = sqrt(pow(X, 2) - 4) / ((2 + X) * (pow(X, 3) - 8));//Решение
+        result = sqrt(pow(X, 2) - 4) / ((2 + X) * (pow(X, 3) - 8));
         std::cout << result;
     }
 }
