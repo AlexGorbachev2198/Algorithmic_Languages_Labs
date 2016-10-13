dbde533cb9f2cbd18f823f823173fc6c919a02d7
AlexGorbachev2198 <Gorbachev2198@gmail.com>
2016-10-13 17:32:56 +0400

    Находится ли точка в квадрате.

--- Lab1_2.cpp
+++ Lab1_2.cpp
@@ -7,20 +7,20 @@ int main() {
     {
         while(R <= 0)
         {
-            std::cout << "Неверная сторона. Введите другую:"<< std::endl; std::cin >> R;//Проверка правильности введенной стороны
+            std::cout << "Неверная сторона. Введите другую:"<< std::endl; std::cin >> R;
         }
     }
     std::cout << "Введите X левой нижней вершины:"<< std::endl; std::cin >> X;
     std::cout << "Введите Y левой нижней вершины:"<< std::endl; std::cin >> Y;
     std::cout << "Введите X точки:"<< std::endl; std::cin >> X1;
-    std::cout << "Введите Y точки:"<< std::endl; std::cin >> Y1; //Ввод значений
+    std::cout << "Введите Y точки:"<< std::endl; std::cin >> Y1;
     if(X1 <= X + R)
     {
         if(X1 >= X)
         {
             if(Y1 <= Y + R)
             {
-                if(Y1 >= Y) std::cout <<"Точка находится в квадрате"<< std::endl; //Проверка и вывод результата
+                if(Y1 >= Y) std::cout <<"Точка находится в квадрате"<< std::endl; 
             }
             else std::cout <<"Точка не находится в квадрате"<< std::endl;
         }
